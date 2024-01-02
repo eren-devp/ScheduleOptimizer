833627ed9a387e34f02450e300fe23e7c8cab80c
Talha <143719883+eren-devp@users.noreply.github.com>
2024-01-02 04:29:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from enum import Enum
 from math import sqrt
 
 MAX_RATING = 100
-RATING_MULTIPLIER = 0.8  # If there is a lesson latter then LATE_HOUR then we will be subtracting LATE_HOUR_PENALTY.
-LATE_HOUR = 12
+LATE_HOUR = 12# If there is a lesson latter then LATE_HOUR then we will be subtracting LATE_HOUR_PENALTY.
 LATE_HOUR_PENALTY = 20
 
 
